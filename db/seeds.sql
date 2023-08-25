76e65f78010d2c7f61a00d27d5e18c70ed0f6c02
Olegreg762 <109924953+Olegreg762@users.noreply.github.com>
2023-08-25 12:26:20 -0500

    update schema.sql, add employees to seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -8,15 +8,17 @@ VALUES
 INSERT INTO role (title, salary, department_id)
 VALUES
 ("Sales Lead", 100000, 1),
-("SalesPerson", 80000, 1)
-("Lead Engineer", 150000, 4)
-("Software Engineer", 120000, 4)
-("Account Manager", 160000, 3)
-("Accountant", 125000, 3)
-("Legal Team Lead", 250000, 2)
+("SalesPerson", 80000, 1),
+("Lead Engineer", 150000, 4),
+("Software Engineer", 120000, 4),
+("Account Manager", 160000, 3),
+("Accountant", 125000, 3),
+("Legal Team Lead", 250000, 2),
 ("Lawyer", 190000, 2);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
-(""),
-("")
+("Camina", "Drummer", 4, NULL),
+("Carlos", "De Baca", 4, 1),
+("Lola", "Shaddid", 2, NULL),
+("Josephus", "Miller", 2, 2);
