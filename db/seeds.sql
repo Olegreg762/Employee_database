1a53c6be6b4c230950a19e67448d82b159a91dd7
Olegreg762 <109924953+Olegreg762@users.noreply.github.com>
2023-08-24 19:32:56 -0500

    create schema, begin seeds, schema map

---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+INSERT INTO department(name, id)
+VALUES 
+("Sales"),
+("Legal"),
+("Finance"),
+("Engineering");
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES
+(""),
+("");
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES
+(""),
+("");
